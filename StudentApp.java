e8a8e6d154a771c60f4a650edcb0eb8137c57873
unknown <akalyaanandhan@gmail.com>
2026-02-19 14:44:02 +0530

    Initial Student Management Project

---
+++ StudentApp.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name);
+    }
+}
+
+public class StudentApp {
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+                    students.add(new Student(id, name));
+                    System.out.println("Student Added!");
+                    break;
+
+                case 2:
+                    System.out.println("\nStudent List:");
+                    for (Student s : students) {
+                        s.display();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+        } while (choice != 3);
+
+        sc.close();
+    }
+}
